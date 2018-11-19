f642bbc032e7a4f972abfcd19ecc984330c32c2c
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-19 12:22:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function(name){
+    return name.toLowerCase()
+  })
+}
+
+function nameToAttributes(arr){
+  return arr.map(function(name){
+    let splitName = name.split(" ")
+    return Object.assign({}, {firstName: splitName[0], lastName: splitName[1]})
+  })
+}
+
+function attributesToPhrase(arr){
+  return arr.map(function(driverObject){
+    return `${driverObject.name} is from ${driverObject.hometown}`
+  })
+}
